d90a7f95d73477f25877614bcb6d815f2b0e4e45
Aliaksei <ylexe97+github.com@gmail.com>
2023-04-17 23:41:41 +0300

    TASK: import-service integration

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,6 +1,6 @@
 const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://dpexjsk6bl.execute-api.us-east-1.amazonaws.com/dev",
   bff: "https://rtoao2fz4k.execute-api.us-east-1.amazonaws.com",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
